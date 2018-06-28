1c11fb298bacc99a7bb530a95b0445e5bd8ec8df
yves7 <40396359+yves7@users.noreply.github.com>
2018-06-28 00:21:05 -0300

    Update versao1.h

--- Versao NURB/versao1.h
+++ Versao NURB/versao1.h
@@ -1,23 +1,8 @@
-/*
- * SimpleNurbs.h
- *
- * Author: Samuel R. Buss
- *
- * Software accompanying the book
- *		3D Computer Graphics: A Mathematical Introduction with OpenGL,
- *		by S. Buss, Cambridge University Press, 2003.
- *
- * Software is "as-is" and carries no warranty.  It may be used without
- *   restriction, but if you modify it, please change the filenames to
- *   prevent confusion between different versions.
- * Bug reports: Sam Buss, sbuss@ucsd.edu.
- * Web page: http://math.ucsd.edu/~sbuss/MathCG
- */
 
-// Function prototypes
+// Inicialização das funções
 
-void myKeyboardFunc( unsigned char key, int x, int y );
-void mySpecialKeyFunc( int key, int x, int y );
+void keyboardFunc( unsigned char key, int x, int y );
+void rotKeyFunc( int key, int x, int y );
 
 void updateScene( void );
 
@@ -30,10 +15,10 @@ void KeyLeft();
 void KeyRight();
 void ResetAnimation();
 void ZeroRotation();
-void ShadeModelToggle();
-void FillModeToggle();
-void MoreUs();
-void LessUs();
-void MoreVs();
-void LessVs();
+void ShadeModelFunc();
+void FillModeFunc();
+void DecU();
+void IncU();
+void DecV();
+void IncV();
 
