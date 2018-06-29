7449485d2ae79ffa5127e64839d2e2dad96f32a0
yves7 <40396359+yves7@users.noreply.github.com>
2018-06-28 23:13:07 -0300

    Add files via upload

---
+++ bezier+catmull (buggado)/versao1.h
@@ -0,0 +1,25 @@
+
+// Inicialização das funções
+
+void keyboardFunc( unsigned char key, int x, int y );
+void rotKeyFunc( int key, int x, int y );
+
+void updateScene( void );
+
+void initRendering();
+void resizeWindow(int w, int h);
+
+void KeyUp();
+void KeyDown();
+void KeyLeft();
+void KeyRight();
+void ResetAnimation();
+void ZeroRotation();
+void ShadeModelFunc();
+void FillModeFunc();
+void DecU();
+void IncU();
+void DecV();
+void IncV();
+
+
